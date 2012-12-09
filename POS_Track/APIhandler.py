03eb0b1dc2975ab87b389eab5e22381a6782f3c4
John Purcell <locke.renard@gmail.com>
2012-12-08 22:03:25 -0700

    added some tools to APIhandler

--- POS_Track/APIhandler.py
+++ POS_Track/APIhandler.py
@@ -10,10 +10,37 @@ corpStarbase="/corp/StarbaseDetail.xml.aspx"
 corpPOSdetail="/corp/StarbaseList.xml.aspx"
 validMask=2&131072&524288
 
+##### DOM Objects #####
+
+##### Queries Handler #####
+##	General data structures for handling multiple API calls to multiple entities
+APIlist
+
+
 def APIload (APIdict, CHARdic):
 	for key in APIdict:
-		keyinfoURL = basepath+keyinfo+"?keyID="+key+"vCode="+APIdict[key]
+		keyinfoURL = "%s%s?keyID=%s&vCode=%s" % basepath,keyinfo,key,APIdict[key]
 		API_keyinfo = minidom.parse(urllib.urlopen(keyinfoURL))
 		
-		if APIvalid (API_keyinfo, "corp", validMask) and APIcorp(API_keyinfo
+		if APIvalid (API_keyinfo, "Corporation", validMask) and !(APIcorp(API_keyinfo).corpName in APIlist):
+			
 
+def APIvalid (domcall, type, mask):
+	#Returns TRUE or FALSE for the API call sent
+	#API: /account/APIKeyInfo.xml.aspx
+	#Type: Account, Character, Corporation
+	#Mask: Desired CAK Access Mask
+	#Expired: checks if given API is expired
+	#IF all members match, TRUE
+	#ELSE FALSE
+	
+	
+class APIcorp(object):
+	#handles some basic corp info from queries
+	
+	def __init__ (self, domcall):
+		
+		self.corpName 
+		self.corpID
+		self.charID
+		self.charName
