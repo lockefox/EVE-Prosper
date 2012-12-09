948c46ecbcf327ea6441a391998aa2c19fb181b0
John Purcell <locke.renard@gmail.com>
2012-12-08 17:23:17 -0700

    added APIhandler headder

--- POS_Track/APIhandler.py
+++ POS_Track/APIhandler.py
@@ -1,7 +1,19 @@
 #!/Python27/python.exe
 
-from xml.dom.minidom import parse, parseString
+from xml.dom import minidom
+
+#####API PATHS#####
+basepath="https://api.eveonline.com"
+keyinfo="/account/APIKeyInfo.xml.aspx"
+corpAsset="/corp/AssetList.xml.aspx"
+corpStarbase="/corp/StarbaseDetail.xml.aspx"
+corpPOSdetail="/corp/StarbaseList.xml.aspx"
+validMask=2&131072&524288
 
 def APIload (APIdict, CHARdic):
-
+	for key in APIdict:
+		keyinfoURL = basepath+keyinfo+"?keyID="+key+"vCode="+APIdict[key]
+		API_keyinfo = minidom.parse(urllib.urlopen(keyinfoURL))
+		
+		if APIvalid (API_keyinfo, "corp", validMask) and APIcorp(API_keyinfo
 
