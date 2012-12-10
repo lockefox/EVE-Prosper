16f5f32175d09cfb25c8513d6102bcf88a1340f6
John Purcell <locke.renard@gmail.com>
2012-12-10 11:10:32 -0700

    added APIcorp class for APIhandler

--- POS_Track/APIhandler.py
+++ POS_Track/APIhandler.py
@@ -8,13 +8,13 @@ keyinfo="/account/APIKeyInfo.xml.aspx"
 corpAsset="/corp/AssetList.xml.aspx"
 corpStarbase="/corp/StarbaseDetail.xml.aspx"
 corpPOSdetail="/corp/StarbaseList.xml.aspx"
-validMask=2&131072&524288
+validMask=2|131072|524288	#Valid API's required for all calls requested
 
 ##### DOM Objects #####
 
 ##### Queries Handler #####
 ##	General data structures for handling multiple API calls to multiple entities
-APIlist
+
 
 
 def APIload (APIdict, CHARdic):
@@ -22,8 +22,8 @@ def APIload (APIdict, CHARdic):
 		keyinfoURL = "%s%s?keyID=%s&vCode=%s" % basepath,keyinfo,key,APIdict[key]
 		API_keyinfo = minidom.parse(urllib.urlopen(keyinfoURL))
 		
-		if APIvalid (API_keyinfo, "Corporation", validMask) and !(APIcorp(API_keyinfo).corpName in APIlist):
-			
+		if APIvalid (API_keyinfo, "Corporation", validMask) and not (APIcorp(API_keyinfo).corpName in APIlist):
+			stuff
 
 def APIvalid (domcall, type, mask):
 	#Returns TRUE or FALSE for the API call sent
@@ -33,14 +33,27 @@ def APIvalid (domcall, type, mask):
 	#Expired: checks if given API is expired
 	#IF all members match, TRUE
 	#ELSE FALSE
+	result = True
+	info = APIcorp(domcall)
 	
+	if info.type != type:
+		result = False
+	if (info.mask & mask) != mask:
+		result = False
+	
+	return result
 	
 class APIcorp(object):
 	#handles some basic corp info from queries
 	
 	def __init__ (self, domcall):
 		
-		self.corpName 
-		self.corpID
-		self.charID
-		self.charName
+		self.corpName	=domcall.getElementsByTagName('row')[0].getAttribute("corporationName")
+		self.corpID		=domcall.getElementsByTagName('row')[0].getAttribute("corporationID")
+		self.charID 	=domcall.getElementsByTagName('row')[0].getAttribute("characterID")
+		self.charName	=domcall.getElementsByTagName('row')[0].getAttribute("characterName")
+		self.mask		=domcall.getElementsByTagName('key')[0].getAttribute("accessMask")
+		self.type		=domcall.getElementsByTagName('key')[0].getAttribute("type")
+		expire			=domcall.getElementsByTagName('key')[0].getAttribute("expires")
+		
+		#return for bool expired.  Empty = false, <current date = false, else true
