598b646bb3a657cf5e96b0e3e13b3f9073bc4291
John Purcell <locke.renard@gmail.com>
2012-12-27 11:15:35 -0700

    updated and completed APIvalidator
    Given (domcall,type,mask_type) will return True if valid
    
    Otherwise returns error string with error info
    - Expired Key
    - Invalid Mask
    - Improper type
    
    mask_type is a string signifying the desired function

--- POS_Track/APIhandler.py
+++ POS_Track/APIhandler.py
@@ -2,6 +2,8 @@
 
 from xml.dom import minidom
 import pos_crunch
+import datetime
+import time
 
 #####API PATHS#####
 basepath="https://api.eveonline.com"
@@ -17,7 +19,8 @@ validMask=2|131072|524288	#Valid API's required for all calls requested
 ##	General data structures for handling multiple API calls to multiple entities
 
 
-def APIvalidator (domcall, which, what):
+def APIvalidator (domcall, which, mask):
+	#Accepts domcall from APIinfo
 	#Returns TRUE if both the API mask is valid AND not past expiry date
 	
 	#API Masks: http://wiki.eve-id.net/APIv2_Page_Index
@@ -79,6 +82,38 @@ def APIvalidator (domcall, which, what):
 		"Trade":			1|4096|1048576|2097152
 	}
 	
+	type = domcall.getElementsByTagName('key')[0].getAttribute("type")
+	CAKmask = int(domcall.getElementsByTagName('key')[0].getAttribute("accessMask"))
+	expiry = domcall.getElementsByTagName('key')[0].getAttribute("expires")
+	
+	if expiry != "":
+		expire_time=time.strptime(expiry,"%Y-%m-%d %H:%M:%S")
+		UTC_now= time.gmtime()
+		if expire_time < UTC_now:
+			return "ERR: API Key Expired"
+	
+	if which == "Character":
+		altwhich = "Account"
+		
+	else:
+		altwhich = ""
+	if type == which or type == altwhich:
+		if type == "Corporation":
+			if (CAKmask & CORP_mask[mask])== CORP_mask[mask]:
+				return True
+			else:
+				return "ERR: Missmatch Mask.  Expected %d, API key is %d" % (CORP_mask[mask],CAKmask)
+		else:	#character
+			if (CAKmask & INDI_mask[mask])== INDI_mask[mask]:
+				return True
+
+			else:
+				return "ERR: Missmatch Mask.  Expected %d, API key is %d" % (INDI_mask[mask],CAKmask)
+	
+	else:
+		return "ERR: Missmatch type.  Expected %s, API key is %s" % which,type
+	
+	
 def APIload (APIdict, CHARdic):
 	for key in APIdict:
 		keyinfoURL = "%s%s?keyID=%s&vCode=%s" % basepath,keyinfo,key,APIdict[key]
@@ -124,7 +159,7 @@ def POS_list(domlist,key,vcode):
 	#returns list of tower objects per corp ID
 	index=0
 	towerlist=[]
-	for dom_row in domcall.getElementsByTagName('row'):
+	for dom_row in domlist.getElementsByTagName('row'):
 		towerinfo = Tower(dom_row,key,vcode)
 		towerlist[index]=towerinfo
 		index += 1
@@ -143,6 +178,8 @@ def cachecheck(domcall):
 API_debug = minidom.parse("APIKeyInfo.xml")
 debugobj = APIcorp(API_debug)
 debug_valid = APIvalid(API_debug,"Corporation", validMask)
-debug_poslist = POS_list(API_debug,1,1)
+debug_valid = APIvalidator(API_debug, "Corporation", "POS")
+print debug_valid
+#debug_poslist = POS_list(API_debug,1,1)
 #print debugobj.type
-print debug_poslist
+#print debug_poslist
