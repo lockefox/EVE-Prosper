a95c0e7db345103d3819e4b895090ed8a3649ed4
John Purcell <locke.renard@gmail.com>
2013-01-26 19:52:09 -0700

    central_dumpcrunch updated.  Loads raw logs to be processed
    TODO: finer parsing for writing to SQL

--- central_dumpcrunch.py
+++ central_dumpcrunch.py
@@ -3,24 +3,44 @@
 ##Designed for UNIX environment
 ##For win/DOS, install cygwin: http://www.cygwin.com/
 
-import csv, sys, math, os, gzip
+import csv, sys, math, os, gzip, getopt, subprocess
 
 ##	Globals	##
 dumpfile = "/central_dumps"
 datafile = "2012-01-01.dump"	#default file for debug
+outfile = "result.csv"
+SQL = 0							#default print = CSV
 pwd_raw = os.popen("pwd")
-pwd = pwd_raw.read()
+pwd = (pwd_raw.read()).rstrip()
 
 def main():
-	parseargs()
-	filepath = "%s/%s" % (pwd,dumpfile)
 	
+	print "running main"
+
+		#READ LIST OF FILES FROM DUMP FOLDER#
+	cmdline = "ls %s%s/*.gz" % (pwd,dumpfile)
+	try:
+		filelist_raw = subprocess.call(cmdline, shell=True)
+	except OSerror:
+		subprocess.call("mkdir %s" % dumpfile, shell=True, stdout=subprocess.PIPE)	
+	filelist_raw = subprocess.Popen(cmdline, shell=True, stdout=subprocess.PIPE)
+	output, err = filelist_raw.communicate()
+	filelist = output.split("\n") #returns list of filenames
+	filelist.pop()	#Removes extra \n empty value from list
+	
+	for filezip in filelist:
+		rawdump = gzip.open(filezip)
+		raw_parse = loadCSV(rawdump)
+		print raw_parse
+		#parse file
+		
+		#print output
 	
 def loadCSV(filename):
 	#accepts string filename and returns dict-dict object of requested file
 	parsed_dump ={}
-	CSV = csv.reader(open(filename)):
-	fields = data.next()
+	CSV = csv.reader(filename)
+	fields = CSV.next()
 	for row in CSV:
 		items = zip(fields, row)	#Strips headder from CSV
 		item = {}
@@ -32,8 +52,32 @@ def loadCSV(filename):
 	return parsed_dump
 	#parsed_dump[orderid]={orderid:###,regionid:###,...}
 	
-def parseargs():
-	global dumpfile, datafile
-	
+def parseargs(argv):
+	global dumpfile, datafile,outfile
+	print "running parseargs"
+	try:
+		opts, args = getopt.getopt(argv, "i:d:o:S:h", ["input=","debug=","output=","SQL","help"])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(1)
+		
+	for opt, arg in opts:
+		if opt in ('-h', '--help'):
+			print "help"
+			usage()
+			sys.exit(1)
+		elif opt in ('-i', '--input='):
+			dumpfile=arg
+		elif opt in ('-d', '--debug='):
+			datafile=arg
+		elif opt in ('-o', '--output='):
+			outfile=arg
+		elif opt in ('-S', "--SQL"):
+			SQL=1
+		else:
+			usage()
+			sys.exit(1)
+			
 if __name__ == "__main__":
+	parseargs(argv=sys.argv)
 	main()
