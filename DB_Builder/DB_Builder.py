d87017d3596e36089aac6551ef35bcb786cd36c3
John Purcell <locke.renard@gmail.com>
2013-02-06 16:20:13 -0700

    updated main program startup functions

--- DB_Builder/DB_Builder.py
+++ DB_Builder/DB_Builder.py
@@ -9,22 +9,26 @@
 #############################
 
 #####		INITS		#####
-import sys,csv, sys, math, os, gzip, getopt, subprocess, math
+import sys,csv, sys, math, os, gzip, getopt, subprocess, math, datetime, time
 import ConfigParser
 import urllib2
 import MySQLdb
 #####		GLOBALS		#####
 config_file = "config.ini"
+config = ConfigParser.ConfigParser()
+config.read(config_file)
+
+startdate=config.get("GLOBALS","startdate")
+enddate=(datetime.datetime.now()).strftime("%Y-%m-%d")
+debug=0
 
 def proginit():
 	#Tests outgoing connections for proper functioning.  Will abort program if any fail
 	
 	#Load config file
-	config = ConfigParser.ConfigParser()
-	config.read(config_file)
+
 	
 	#Verify internet connections#
-	
 	try:	#eve-central
 		urllib2.urlopen(urllib2.Request(config.get("EVE_CENTRAL","central_path")))
 	except URLERROR as e:
@@ -40,7 +44,6 @@ def proginit():
 		sys.exit(4)
 		
 	#Verify DB connection
-	
 	try:
 		con = MySQLdb.connect(host=config.get("GLOBALS","db_IP"), user=config.get("GLOBALS","db_username"), passwd=config.get("GLOBALS","db_pw"), port=int(config.get("GLOBALS","db_port")), db=config.get("GLOBALS","eve_marketdata"))
 		cur = con.cursor()
@@ -53,4 +56,59 @@ def proginit():
 		sys.exit(5)
 		
 	con.close()
+
+def parseargs():
+	global filepath,outputpath
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hs:ed:",["startdate=","enddate=","debug"])
+	except getopt.GetoptError:
+		print 'log2ppd.py -i <filepath>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			help()
+		elif opt in ("-s", "--startdate"):
+			try:
+				time.strptime(arg, "%Y-%m-%d")
+			except ValueError:
+				print "Invalid startdate date format.  Expected YYYY-MM-DD"
+				sys.exit(2)
+				
+			startdate=arg
+		elif opt in ("-e", "--enddate"):
+			try:
+				time.strptime(arg, "%Y-%m-%d")
+			except ValueError:
+				print "Invalid enddate date format.  Expected YYYY-MM-DD"
+				sys.exit(2)
+			
+			enddate=arg
+		
+		elif opt in ("-d","--debug"):
+			debug=1
+		else:
+			help()
+
+		
+def help():
+	print """DB_Builder.py
+		Used to create/maintain local DB to mirror the one Prosper hosts in Appengine
+		this is to minimize debug on costly server time inside appengine
 	
+	OPTIONS:
+		--startdate: YYYY-MM-DD	default=Crucible release (2011-11-29)
+		(--enddate: YYYY-MM-DD)	default=today
+		(--debug)				default=false
+	"""
+	sys.exit(1)
+	
+def dbinit():
+	db = MySQLdb.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWD, port=int(DATABASE_PORT), db=DATABASE_NAME)
+	cursor = db.cursor()
+	return cursor
+	
+if __name__ == "__main__":
+	proginit()
+	parseargs()
+	cursor = dbinit()
+	main()
