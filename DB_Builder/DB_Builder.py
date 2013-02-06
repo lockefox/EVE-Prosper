ba78269449c5d9ec89c7884c742590dd17bf531d
John Purcell <locke.renard@gmail.com>
2013-02-06 10:39:04 -0700

    started DB_Builder
    wrapper for building a local copy of the PROSPER DB for debug

---
+++ DB_Builder/DB_Builder.py
@@ -0,0 +1,9 @@
+#!/Python27/python.exe
+
+#####	DB_BUILDER.PY	#####
+#	A wrapper to build the Prosper DB's locally for the purpose of debug
+#
+#	Coalesses the various DB building tools into one easy script
+#	Controlled by "config.ini" file + some limited cmd line args
+#
+#############################
