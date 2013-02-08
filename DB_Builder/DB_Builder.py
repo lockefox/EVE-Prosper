5204353c4c75eb7856c5fcdb302166ddf10e7c06
John Purcell <locke.renard@gmail.com>
2013-02-08 16:46:43 -0700

    offloaded program_init to its own module
    allows globals for Queue/DB_cursor

--- DB_Builder/DB_Builder.py
+++ DB_Builder/DB_Builder.py
@@ -14,113 +14,14 @@ import ConfigParser
 import urllib2
 import MySQLdb
 import threading,Queue
+import init
 #####		GLOBALS		#####
-config_file = "config.ini"
-config = ConfigParser.ConfigParser()
-config.read(config_file)
 
-startdate=datetime.datetime.strptime(config.get("GLOBALS","startdate"),"%Y-%m-%d")
-enddate=(datetime.datetime.now()).strftime("%Y-%m-%d")
-debug=config.get("DEBUG","debug")
-
-queue = Queue.Queue()
-
-def proginit():
-	#Tests outgoing connections for proper functioning.  Will abort program if any fail
-	
-	#Load config file
-
-	
-	#Verify internet connections#
-	try:	#eve-central
-		urllib2.urlopen(urllib2.Request(config.get("EVE_CENTRAL","central_path")))
-	except urllib2.URLError as e:
-		print "Unable to query EVE-Central Dump repository at %s" % config.get("EVE_CENTRAL","central_path")
-		print e.code
-		sys.exit(4)
-	except urllib2.HTTPError as er:
-		print "Unable to query EVE-Central Dump repository at %s" % config.get("EVE_CENTRAL","central_path")
-		print er.code
-		sys.exit(4)
-		
-	try:	#zkillboard
-		urllib2.urlopen(urllib2.Request(config.get("TOASTER_CFG","toaster_path")))
-	except urllib2.URLError as e:
-		print "Unable to query Killboard repository at %s" % config.get("TOASTER_CFG","toaster_path")
-		print e.code
-		sys.exit(4)
-	except urllib2.HTTPError as er:
-		print "Unable to query Killboard repository at %s" % config.get("TOASTER_CFG","toaster_path")
-		print er.code
-		sys.exit(4)
-	#Verify DB connection
-	try:
-		con = MySQLdb.connect(host=config.get("GLOBALS","db_IP"), user=config.get("GLOBALS","db_username"), passwd=config.get("GLOBALS","db_pw"), port=int(config.get("GLOBALS","db_port")), db=config.get("GLOBALS","root_dbname"))
-		cur = con.cursor()
-		cur.execute("SELECT VERSION()")
-		data = cur.fetchone()
-		print "DB Version: %s" % data
-	except MySQLdb.Error, e:
-		print "Unable to connect to destination database"
-		print "ERROR %d: %s" % (e.args[0],e.args[1])
-		sys.exit(5)
-		
-	con.close()
-
-def parseargs():
-	global filepath,outputpath
-	try:
-		opts, args = getopt.getopt(sys.argv[1:],"hs:ed:",["startdate=","enddate=","debug"])
-	except getopt.GetoptError:
-		print 'log2ppd.py -i <filepath>'
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-			help()
-		elif opt in ("-s", "--startdate"):
-			try:
-				time.strptime(arg, "%Y-%m-%d")
-			except ValueError:
-				print "Invalid startdate date format.  Expected YYYY-MM-DD"
-				sys.exit(2)
-				
-			startdate=datetime.strptime(arg,"%Y-%m-%d")
-		elif opt in ("-e", "--enddate"):
-			try:
-				time.strptime(arg, "%Y-%m-%d")
-			except ValueError:
-				print "Invalid enddate date format.  Expected YYYY-MM-DD"
-				sys.exit(2)
-			
-			enddate=datetime.strptime(arg,"%Y-%m-%d")
-		
-		elif opt in ("-d","--debug"):
-			debug=1
-		else:
-			help()
-
-		
-def help():
-	print """DB_Builder.py
-		Used to create/maintain local DB to mirror the one Prosper hosts in Appengine
-		this is to minimize debug on costly server time inside appengine
-	
-	OPTIONS:
-		--startdate: YYYY-MM-DD	default=Crucible release (2011-11-29)
-		(--enddate: YYYY-MM-DD)	default=today
-		(--debug)				default=false
-	"""
-	sys.exit(1)
-	
-def dbinit():
-	db = MySQLdb.connect(host=DATABASE_HOST, user=DATABASE_USER, passwd=DATABASE_PASSWD, port=int(DATABASE_PORT), db=DATABASE_NAME)
-	cursor = db.cursor()
-	return cursor
 
 def main():
 	print "main"
 if __name__ == "__main__":
-	proginit()
-	parseargs()
-	cursor = dbinit()
+	init.proginit()
+	init.parseargs()
+	dbinit()
 	main()
--- DB_Builder/eve_central.py
+++ DB_Builder/eve_central.py
@@ -3,16 +3,21 @@
 import Queue,threading
 import urllib2
 import date,datetime
+import MySQLdb
+import ConfigParser
+import init
+
 
 def datelist(startdate,enddate,cursor):
 	#takes startdate/enddate and checks against the existing db to populate a list of missing dates
 	list_of_dates=[]
 	mindate = enddate
+	source_db = config.get("EVE_CENTRAL","datesource")
 		#Write a list of ALL DAYS possible
 	while mindate != startdate:
 		list_of_dates.append(mindate.strftime("%Y-%m-%d"))	#Write string representation of datetime to list
 		mindate += datetime.timedelta(days=1)
 		
 		#remove existing elements
-	
+	existing_set
 	return list_of_dates
