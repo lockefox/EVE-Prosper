18078a5a532cec9a8a5e0d1229bd0d8f4645082f
John Purcell <locke.renard@gmail.com>
2013-02-26 13:54:00 -0700

    added file for toaster (KB processor)

---
+++ DB_Builder/toaster.py
@@ -0,0 +1,9 @@
+#!/Python27/python.exe
+import datetime
+import init
+#https://zkillboard.com/information/api/
+
+def tozKBtime(to_convert):
+	#takes to_convert as datetime 
+	
+	return zKB_starttime
