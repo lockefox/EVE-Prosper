d2cabab4c398982e6d107d8476a12ddccd0d7404
John Purcell <locke.renard@gmail.com>
2013-03-05 13:34:43 -0700

    added psudocode outlines for toaster.py

--- DB_Builder/toaster.py
+++ DB_Builder/toaster.py
@@ -5,10 +5,27 @@ import init
 
 def tozKBtime(to_convert):
 	#takes to_convert as datetime 
-	
-	return zKB_starttime
+	stringtime = to_convert.datetime.strftime("%Y%m$d")
+	zKBtime = "/starttime/%s0000/endtime/%s2359" % (stringtime,stringtime)
+	return zKBtime
 	
 def query_range(daterange):
 	#takes list of desired dates and returns (starttime,endtime) list of queries
 	
 	return date_list
+	
+	
+class kills_query(object):
+
+	def __init__(self,query_string):
+		self.kill_pages=pageloader
+		self.query_string=query_string
+		
+	def pageloader(self):
+		#fetch page1
+		zKB_json=zKB_fetch(self.query_string)
+		
+		#if killID count = 200 (>199)
+		
+		#query new pages until count <200
+		
