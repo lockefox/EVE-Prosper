4601fbf0c3c94fb7a9fb38d443e43a72a1650170
John Purcell <jpurcell@micron.com>
2013-07-23 14:00:20 -0600

    added basic global inits to init.py

--- Toaster/toaster_init.py
+++ Toaster/toaster_init.py
@@ -9,3 +9,17 @@ import threading,Queue
 config_file = "config.ini"
 config = ConfigParser.ConfigParser()
 config.read(config_file)
+
+startdate=datetime.datetime.strptime(config.get("GLOBALS","startdate"),"%Y-%m-%d")
+enddate=(datetime.datetime.now())
+debug=config.get("DEBUG","debug")
+
+def proginit():
+	try: #kb-site
+		urllib2.urlopen(urllib2.Request(config.get("TOASTER_CFG","toaster_path")))
+	except urllib2.URLError as e:
+		print "Unable to query %s" % config.get("TOASTER_CFG","toaster_path")
+		print e.code
+		sys.exit(4)	
+		
+	
