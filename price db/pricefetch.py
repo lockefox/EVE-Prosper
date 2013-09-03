5f7b360c6aec3794db07d9ab8178f005bccabb54
John Purcell <jpurcell@micron.com>
2013-09-03 14:19:20 -0600

    fixed bugs.  Initial feature-set complete

--- price db/pricefetch.py
+++ price db/pricefetch.py
@@ -29,7 +29,7 @@ def init():
 	if (csv_only==0 and sql_init_only==0):	
 		global db_name,db_cursor,db_schema, db
 		db_name="pricedata"
-		db_schema="sdretribution11"
+		db_schema="odyssey-1.0-89097"
 		db_IP="127.0.0.1"
 		db_user="root"
 		db_pw="bar"
@@ -46,13 +46,13 @@ def init():
 			`typeName` varchar(45) NOT NULL,\
 			`typeID` int(8) NOT NULL,\
 			`source` varchar(8) NOT NULL,\
-			`priceMax` float(8,2) DEFAULT NULL,\
-			`priceMin` float(8,2) DEFAULT NULL,\
-			`priceAverage` float(8,2) DEFAULT NULL,\
+			`priceMax` float(12,2) DEFAULT NULL,\
+			`priceMin` float(12,2) DEFAULT NULL,\
+			`priceAverage` float(12,2) DEFAULT NULL,\
 			`volume` int(16) DEFAULT NULL,\
 			`orders` int(16) DEFAULT NULL,\
-			`priceOpen` float(8,2) DEFAULT NULL,\
-			`priceClose` float(8,2) DEFAULT NULL,\
+			`priceOpen` float(12,2) DEFAULT NULL,\
+			`priceClose` float(12,2) DEFAULT NULL,\
 			PRIMARY KEY (`date`,`locationID`,`typeID`,`source`))\
 			ENGINE=InnoDB DEFAULT CHARSET=latin1" % db_name)
 		except MySQLdb.OperationalError as e:
@@ -65,29 +65,30 @@ def init():
 		print "DB connection:\t\t\tSKIPPED"
 		
 	##TEST Internet connection and resource connection##
-	try:	#EVE-Marketdata.com connection
-		urllib2.urlopen(urllib2.Request(EMD_base))
-	except urllib2.URLError as e:
-		print "Unable to connect to EMD at %s" % EMD_base
-		print e.code
-		sys.exit(4)
-	except urllib2.HTTPError as er:
-		print "Unable to connect to EMD at %s" % EMD_base
-		print er.code
-		sys.exit(4)
-	print "EVE-Marketdata connection:\tGOOD"
-	
-	try:	#EVE-Central.com connection
-		urllib2.urlopen(urllib2.Request(EC_base))
-	except urllib2.URLError as e:
-		print "Unable to connect to EVE-Central at %s" % EC_base
-		print e.code
-		sys.exit(4)
-	except urllib2.HTTPError as er:
-		print "Unable to connect to EVE-Central at %s" % EC_base
-		print er.code
-		sys.exit(4)
-	print "EVE-Central connection:\t\tGOOD"
+	if EMD_parse == 1:
+		try:	#EVE-Marketdata.com connection
+			urllib2.urlopen(urllib2.Request(EMD_base))
+		except urllib2.URLError as e:
+			print "Unable to connect to EMD at %s" % EMD_base
+			print e.code
+			sys.exit(4)
+		except urllib2.HTTPError as er:
+			print "Unable to connect to EMD at %s" % EMD_base
+			print er.code
+			sys.exit(4)
+		print "EVE-Marketdata connection:\tGOOD"
+	if EC_parse == 1:
+		try:	#EVE-Central.com connection
+			urllib2.urlopen(urllib2.Request(EC_base))
+		except urllib2.URLError as e:
+			print "Unable to connect to EVE-Central at %s" % EC_base
+			print e.code
+			sys.exit(4)
+		except urllib2.HTTPError as er:
+			print "Unable to connect to EVE-Central at %s" % EC_base
+			print er.code
+			sys.exit(4)
+		print "EVE-Central connection:\t\tGOOD"
 	
 def parseargs():
 	try:
@@ -153,40 +154,75 @@ def EMD_proc():
 		progress[0]+=1
 		if len(item_query_group)==items_per_query or (progress[0] + items_per_query) > len(item_todo):
 			EMD_URL = "http://api.eve-marketdata.com/api/item_history2.json?char_name=Lockefox&region_ids=%s&type_ids=%s&days=%s" % (region_query,",".join(item_query_group),days_query)
+			print EMD_URL
+			print "----"
 			#print ",".join(item_query_group)
 			response = urllib2.urlopen(EMD_URL).read()
 			query_result = json.loads(response)
 			hold_results=[]
-			real_results=[]
+			#real_results=[]
 			item_dict={}
-			for individual_item in item_query_group:	#write to SQL by individual item
+			for row_ittr in query_result["emd"]["result"]:	#Parse json into organized object
+				result_line=result_line = [row_ittr["row"]["date"],row_ittr["row"]["regionID"],lookup["types"][row_ittr["row"]["typeID"]],\
+					row_ittr["row"]["typeID"],"EMD",row_ittr["row"]["highPrice"],row_ittr["row"]["lowPrice"],row_ittr["row"]["avgPrice"],\
+					row_ittr["row"]["volume"],row_ittr["row"]["orders"],None,None]
+				if row_ittr["row"]["typeID"] in item_dict:
+					hold_line=item_dict[row_ittr["row"]["typeID"]]
+					hold_line.append(result_line)
+					item_dict[row_ittr["row"]["typeID"]]=hold_line
+				else:
+					item_dict[row_ittr["row"]["typeID"]]=[]
+					item_dict[row_ittr["row"]["typeID"]].append(result_line)
+				#key,value
+			for item,results in item_dict.iteritems():		#Backfill missing dates, and write out to SQL
+				#print results
+				real_results=[]
 				
-				for row_ittr in query_result["emd"]["result"]:
-					if int(row_ittr["row"]["typeID"]) != int(individual_item):
-						continue
-					result_line = [row_ittr["row"]["date"],row_ittr["row"]["regionID"],lookup["types"][row_ittr["row"]["typeID"]],row_ittr["row"]["typeID"],"EMD",row_ittr["row"]["highPrice"],row_ittr["row"]["lowPrice"],row_ittr["row"]["avgPrice"],row_ittr["row"]["volume"],row_ittr["row"]["orders"],None,None]
-					#print result_line
-					hold_results.append(result_line)
-				#print hold_results
-				#print dates_todo
-				item_dic
-				date_indx=0
-				previous_entry=hold_results[0] #IF T0 = null, then pass back earliest value to T0
-				for result_ittr in hold_results:	#Clean up missing values
-					print result_ittr
-					print date_indx
-					if result_ittr[0]== dates_todo[date_indx]:	#found value
-						real_results.append(result_ittr)
-						date_indx+=1
-						previous_entry=result_ittr
-						continue
-					else:										#Did not find value, use previous
-						real_results.append(previous_entry)
-						date_indx+=1
-						previous_entry=result_ittr
-					
+
+				date_index=0
+				previous_line=results[0]
+				for date in dates_todo:
+					line_found=0
+					for line in results:
+						if line[0]==date:
+							tmp_result = [date,line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11]]
+							real_results.append(tmp_result)
+							previous_line=line
+							line_found=1
+							break
+					if line_found==0:
+						tmp_result=[date,previous_line[1],previous_line[2],previous_line[3],"EMD_mod",previous_line[5],previous_line[6],previous_line[7],previous_line[8],previous_line[9],previous_line[10],previous_line[11]]
+						real_results.append(tmp_result)
+				
 				write_sql(real_results)
-			item_query=""
+						
+			#for individual_item in item_query_group:	#write to SQL by individual item
+			#	
+			#	for row_ittr in query_result["emd"]["result"]:
+			#		if int(row_ittr["row"]["typeID"]) != int(individual_item):
+			#			continue
+			#		result_line = [row_ittr["row"]["date"],row_ittr["row"]["regionID"],lookup["types"][row_ittr["row"]["typeID"]],row_ittr["row"]["typeID"],"EMD",row_ittr["row"]["highPrice"],row_ittr["row"]["lowPrice"],row_ittr["row"]["avgPrice"],row_ittr["row"]["volume"],row_ittr["row"]["orders"],None,None]
+			#		#print result_line
+			#		hold_results.append(result_line)
+			#	#print hold_results
+			#	#print dates_todo
+			#	date_indx=0
+			#	previous_entry=hold_results[0] #IF T0 = null, then pass back earliest value to T0
+			#	for result_ittr in hold_results:	#Clean up missing values
+			#		print result_ittr
+			#		print date_indx
+			#		if result_ittr[0]== dates_todo[date_indx]:	#found value
+			#			real_results.append(result_ittr)
+			#			date_indx+=1
+			#			previous_entry=result_ittr
+			#			continue
+			#		else:										#Did not find value, use previous
+			#			real_results.append(previous_entry)
+			#			date_indx+=1
+			#			previous_entry=result_ittr
+			#		
+			#	write_sql(real_results)
+			item_query_group=[]
 			display_progress(progress)
 def display_progress(progress_list):
 	progress_string = "%s of %s complete" % (progress_list[0], progress_list[1])
@@ -199,15 +235,15 @@ def write_sql(result_list):
 	result_indx=0
 	for line in result_list:
 		print_list=[]
-		if result_indx==0:
-			result_indx+=1
-			continue
+		#if result_indx==0:
+		#	result_indx+=1
+		#	continue
 		for element in line:
 			if element == None:
 				print_list.append("NULL")
 			else:
 				print_list.append(element)
-		tmp_str="REPLACE %s.%s (date,locationID,typeName,typeID,source,priceMax,priceMin,priceAverage,volume,orders,priceOpen,priceClose) VALUES('%s',%s,'%s',%s,'%s',%s,%s,%s,%s,%s,%s,%s);" %(db_schema,db_name,\
+		tmp_str="REPLACE %s (date,locationID,typeName,typeID,source,priceMax,priceMin,priceAverage,volume,orders,priceOpen,priceClose) VALUES('%s',%s,'%s',%s,'%s',%s,%s,%s,%s,%s,%s,%s);" %(db_name,\
 			print_list[0], print_list[1], print_list[2], print_list[3], print_list[4],\
 			print_list[5], print_list[6], print_list[7], print_list[8], print_list[9],\
 			print_list[10], print_list[11])
