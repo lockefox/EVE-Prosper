f2ce7bd12201ea5422e3c94e59851fd4ee661093
John Purcell <jpurcell@micron.com>
2013-09-30 16:06:01 -0600

    updated zkb_fetch init()
    Checks existing DB and zKB connection
    
    TODO: DB exists, but bins don't match case

--- price db/zkb_fetch.py
+++ price db/zkb_fetch.py
@@ -27,7 +27,7 @@ db_cursor=None
 def init():
 	global db_name,db_schema,db,db_cursor
 	db_name="killdata"
-	db_schema="sdretribution11"
+	db_schema="odyssey-1.1-91288"
 	db_IP="127.0.0.1"
 	db_user="root"
 	db_pw="bar"
@@ -40,22 +40,57 @@ def init():
 	
 	db_cursor.execute("SHOW TABLES LIKE '%s'" % db_name)
 	db_exists = len(db_cursor.fetchall())	#zero if not initialized
-	#date,itemID,item_category,item_name,[locationbin]
+	#date,itemID,item_name,item_category,[locationbin]
 	db_cols_match=1
 	if db_exists:
-		db_cursor.execute("SELECT COLUMN_NAME FROM INFORATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='%s' AND TABLE_NAME='%s'" % (db_schema,db_name))
+		db_cursor.execute("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS`  WHERE `TABLE_SCHEMA`='%s' AND `TABLE_NAME`='%s'" % (db_schema,db_name))
 		existing_cols = db_cursor.fetchall()
-		if (len(existing_cols)-4 != len(systems["systemlist"].keys()): #check if bin count lines up
+		if (len(existing_cols)-4 != len(systems["systemlist"].keys())): #check if bin count lines up
+			db_cols_match=0
 			print "Number of columns in EXISTING table does not match bins in %s" % systemlist
 			print "please manually DROP %s from %s" % db_name,db_schema
 			
-		for bin,sys_list in systems["systemlist"].itteritems():
-			if (bin not in existing_cols):
-				db_cols_match=0
-				print "%s not found in existing db" % bin
-				print "please manually DROP %s from %s" % db_name,db_schema
-				sys.exit(2)
+			#CASE FOR DB exists, but BINS might have changed
+		#for bin,sys_list in systems["systemlist"].iteritems():
+		#	aug_bin = "('%s',)" % bin
+		#	if (aug_bin not in existing_cols):
+		#		db_cols_match=0
+		#		print "%s not found in existing db" % aug_bin
+		#		print "please manually DROP %s from %s" % db_name,db_schema
+		#		sys.exit(2)
 	
+	else:	#Initialize DB
+		bin_str=""
+		for bin,sys_list in systems["systemlist"].iteritems():
+			bin_str+="`%s` int(16) DEFAULT NULL,"%(bin)
+		try:
+			db_cursor.execute( "CREATE TABLE %s (\
+				`date` date NOT NULL,\
+				`typeID` int(8) NOT NULL,\
+				`typeName` varchar(100) NOT NULL,\
+				`typeCategory` int(8) NOT NULL,\
+				%s\
+				PRIMARY KEY (`date`,`typeID`))\
+				ENGINE=InnoDB DEFAULT CHARSET=latin1" % (db_name,bin_str))
+		except MySQLdb.OperationalError as e:
+			if (e[0] == 1050): #Table Already Exists
+				print "%s table already created" % db_name
+			else:
+				raise e		
+	print "DB Connection:\t\t\tGOOD"
+
+	try:	#EVE-Marketdata.com connection
+		urllib2.urlopen(urllib2.Request(zkb_base))
+	except urllib2.URLError as e:
+		print "Unable to connect to zKB at %s" % zkb_base
+		print e.code
+		sys.exit(4)
+	except urllib2.HTTPError as er:
+		print "Unable to connect to zKB at %s" % zkb_base
+		print er.code
+		sys.exit(4)
+	print "zKillboard connection:\tGOOD"
+
 def main():
 	#parseargs()
 	init()
