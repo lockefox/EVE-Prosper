6c400bdf0a8aa28562053e38ddd5e7445ef90c30
Lockefox <locke.renard@gmail.com>
2013-10-25 10:23:49 -0600

    added table init for materialdestroyed tool

--- DB_Builder/materialdestroyed_bridge.py
+++ DB_Builder/materialdestroyed_bridge.py
@@ -44,3 +44,53 @@ kill_db_pw = conf.get("GLOBALS","db_pw")
 kill_db_port = int(conf.get("GLOBALS","db_port"))
 kill_db_cursor = None
 kill_db = None
+
+def init:
+	global db_cursor, db
+	global kill_db_cursor, kill_db
+	
+	#Two cursors to allow for disperate hosting (though I can't imagine why)
+	try:
+		kill_db = MySQLdb.connect(host=kill_db_IP, user=kill_db_user, passwd=kill_db_pw, port=kill_db_port, db=kill_db_name)	
+		kill_db_cursor = kill_db.cursor()	
+	except MySQLdb.Error as er:
+		print "Cannot connect to kill database"
+		sys.exit(2)
+
+	#check if kill data table exists
+	try:
+		kill_db_cursor.execute("SELECT 1 FROM %s LIMIT 1" % kill_db_table)
+	except MySQLdb.OperationalError as e:
+		print "%s does not exist.  Initialize with zkb_scraper" % kill_db_table
+		sys.exit(2)
+	except MySQLdb.ProgrammingError as er:
+		print "%s does not exist.  Initialize with zkb_scraper" % kill_db_table
+		sys.exit(2)
+	print "Kill DB Connection:\t\tGOOD"
+
+	try:
+		db = MySQLdb.connect(host=db_IP, user=db_user, passwd=db_pw, port=db_port, db=db_name)	
+		db_cursor = db.cursor()
+	except MySQLdb.Error as er:
+		print "Cannot connect to kill database"
+		sys.exit(2)
+		
+	try:
+		db_cursor.execute("CREATE TABLE %s (\
+			`date` date NOT NULL,\
+			`week` varchar(8) NOT NULL,\
+			`typeID` int(8) NOT NULL,\
+			`typeGroup` int(8) NOT NULL,\
+			`systemID` int(8) NOT NULL,\
+			`destroyed` bigint(32) DEFAULT 0,\
+			PRIMARY KEY (`date`,`typeID`,`systemID`))\
+			ENGINE=InnoDB DEFAULT CHARSET=latin1" % db_name)
+	except MySQLdb.OperationalError as e:
+		if (e[0] == 1050): #Table Already Exists
+			print "%s table already created" % db_name
+		else:
+			raise e		
+			sys.exit(2)
+	print "Material DB connection:\t\tGOOD"
+	
+	
