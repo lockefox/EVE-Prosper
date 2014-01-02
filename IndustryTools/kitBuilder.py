df38030639251e77e8e6ef2a29197e13c2e24435
lockefox <locke.renard@gmail.com>
2014-01-02 15:06:42 -0700

    added kitbuilder

---
+++ IndustryTools/kitBuilder.py
@@ -0,0 +1,11 @@
+#!/Python27/python.exe
+
+import sys, gzip, StringIO, csv, sys, math, os, getopt, subprocess, math, datetime, time, json, socket
+import urllib2
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import MySQLdb
+import ConfigParser
+
+import eve_characters
+
