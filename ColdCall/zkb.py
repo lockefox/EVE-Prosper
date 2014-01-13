8325cacf1f2472213e283de9c5c98ac0c8069438
lockefox <locke.renard@gmail.com>
2014-01-13 13:45:09 -0700

    added first-pass fetchResults() function

--- ColdCall/zkb.py
+++ ColdCall/zkb.py
@@ -9,7 +9,14 @@ conf = ConfigParser.ConfigParser()
 conf.read(["init.ini", "init_local.ini"])
 
 base_query = conf.get("ZKB","base_query")
-subquery_limit = conf.get("ZKB","subquery_limit")
+query_limit = int(conf.get("ZKB","query_limit"))
+subquery_limit = int(conf.get("ZKB","subquery_limit"))
+retry_limit = int(conf.get("ZKB","retry_limit"))
+default_sleep = int(conf.get("ZKB","default_sleep"))
+User_Agent = conf.get("GLOBALS","user_agent")
+
+sleepTime = query_limt/(24*60*60)
+
 valid_modifiers = (
 	"kills",
 	"losses",
@@ -49,6 +56,10 @@ class Query(object):
 			self.IDcount +=2
 			#do load into queryElements
 		self.__initialized == True
+		
+	def fetch(self):
+		return fetchResults(self)
+		
 	def orderDirection(self,dir):
 		dirLower = dir.lower()
 		if dirLower not in ("asc","desc"):
@@ -229,7 +240,126 @@ class Query(object):
 				query_modifiers = "%s%s/%s/" % (query_modifiers,key,value)
 				
 		return "%s%s" % (self.address,query_modifiers)
+
+def fetchLatestKillID ():
+	singleton_query = Query("api-only/solo/kills/limit/1/")
+	kill_obj = fetchResults(singleton_query,1)
+	
+	return kill_obj[0]["killID"]
+	
+def latestKillID(kill_obj):
+	earliest_time = datetime.strptime("1970-01-01 00:00:00","%Y-%m-%d %H:%M:%S")	#epoch 0
+	latest_ID = 0
+	for kill in kill_obj:
+		killTime = datetime.strptime(kill["killTime"],"%Y-%m-%d %H:%M:%S")
+		if killTime > earliest_time:
+			earliest_time = killTime
+			latest_ID = kill["killID"]
+			
+	return latest_ID
+	
+def earliestKillID(kill_obj):
+	latest_time = datetime.utcnow()
+	earliest_ID = 0
+	for kill in kill_obj:
+		killTime = datetime.strptime(kill["killTime"],"%Y-%m-%d %H:%M:%S")
+		if killTime < latest_time:
+			latest_time = killTime
+			earliest_ID = kill["killID"]
+			
+	return earliest_ID
+	
+def fetchResults(queryObj,scrapeHeader=0):
+	zkb_url = str(queryObj)
+	
+	request = urllib2.Request(zkb_url)
+	request.add_header('Accept-Encoding','gzip')
+	request.add_header('User-Agent',User_Agent)
+	
+	#log query
+	
+	for tries in range (0,retry_limit):
+		time.sleep(sleepTime*(tries+1))
+		try:
+			opener = urllib2.build_opener()
+			header_hold = urllib2.urlopen(request).headers
+			raw_zip = opener.open(request)
+			dump_zip_stream = raw_zip.read()
+		except urllib2.HTTPError as e:
+			#log_filehandle.write("%s: %s\n" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), e))
+			print "retry %s: %s" %(zkb_addr,tries+1)
+			continue
+		except urllib2.URLError as er:
+			#log_filehandle.write("%s: %s\n" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), er))
+			print "URLError.  Retry %s: %s" %(zkb_addr,tries+1)
+			continue
+		except socket.error as err:
+			#log_filehandle.write("%s: %s\n" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), err))
+			print "Socket Error.  Retry %s: %s" %(zkb_addr,tries+1)
+		_snooze(header_hold)
 		
+		try:
+			dump_IOstream = StringIO.StringIO(dump_zip_stream)
+			zipper = gzip.GzipFile(fileobj=dump_IOstream)
+			JSON_obj = json.load(zipper)
+		except ValueError as errr:
+			#log_filehandle.write("%s: %s\n" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), errr))
+			print "Empty response.  Retry %s: %s" %(zkb_addr,tries+1)
+			
+		else:
+			break
+	else:
+		print header_hold
+		sys.exit(2)
+		
+	return JSON_obj
+	
+def _snooze(http_header,multiplier=1):
+	global query_limit, sleepTime
+	
+	try:
+		query_limit  = int(http_header["X-Bin-Max-Requests"])
+		request_used = int(http_header["X-Bin-Request-Count"])
+	except KeyError as e:
+		sleepTime = query_limit/(24*60*60)*multiplier
+		return sleepTime
+	sleepTime = 0
+	if request_used/max_request <= 0.5:
+		return sleepTime
+	elif request_used/max_request > 0.9:
+		sleepTime = query_limit/(24*60*60)*multiplier*2
+		return sleepTime	
+	elif request_used/max_request > 0.75:
+		sleepTime = query_limit/(24*60*60)*multiplier
+		return sleepTime
+	elif request_used/max_request > 0.5:
+		sleepTime = query_limit/(24*60*60)*multiplier*0.5
+		return sleepTime
+	else:
+		sleepTime = query_limit/(24*60*60)*multiplier
+		return sleepTime
+		
+def _snoozeSetter(http_header):
+	global query_limit,snooze_timer
+	
+	try:
+		query_limit = int(http_header["X-Bin-Max-Requests"])
+	except KeyError as e:
+		print "WARNING: http_header key 'X-Bin-Max-Requests' not found"
+		query_limit = int(conf.get("ZKB","query_limit"))
+		snooze_timer = query_limit/(24*60*60)	#requests per day
+		
+	
+def _politeSnooze(http_header):
+	global snooze_timer
+	call_sleep = 0
+	conn_allowance = int(http_header["X-Bin-Attempts-Allowed"])
+	conn_reqs_used = int(http_header["X-Bin-Requests"])	
+	conn_sleep_time= int(header["X-Bin-Seconds-Between-Request"])
+	
+	if (conn_reqs_used+1)==conn_allowance:
+		time.sleep(conn_sleep_time)
+
 def main():
 	newQuery = Query()
 	newQuery2 = Query("api-only/characterID/628592330/losses/")
