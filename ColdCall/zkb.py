91fe54931d83e9b7c9bf465fa286ff50338ff5b4
lockefox <locke.renard@gmail.com>
2014-01-13 12:17:52 -0700

    replaced query elements with valid modules
    __setattr__ was too convoluted.  Replaced with flat definitions

--- ColdCall/zkb.py
+++ ColdCall/zkb.py
@@ -1,6 +1,6 @@
 #!/Python27/python.exe
 
-import sys, gzip, StringIO, csv, sys, math, os, getopt, subprocess, math, datetime, time, json, socket,six
+import sys, gzip, StringIO, csv, sys, math, os, getopt, subprocess, math, datetime, time, json, socket
 import urllib2
 import MySQLdb
 import ConfigParser
@@ -38,11 +38,15 @@ class QueryException(Exception):
 		else:
 			return "Useless generic Exception"
 			
-class Query():
-	def __init__ (self):
+class Query(object):
+	__initialized = False
+	def __init__ (self, queryArgs=""):
 		self.address = base_query
+		self.queryArgs = queryArgs
 		self.queryElements = {}
 		self.IDcount = 0
+		
+		self.__initialized == True
 	def orderDirection(self,dir):
 		dirLower = dir.lower()
 		if dirLower not in ("asc","desc"):
@@ -95,69 +99,128 @@ class Query():
 	def dateConv (self,date):
 		date_str = date.strftime("%Y%m%d%H%M")
 		return date_str
-		
-
-	def __getattr__ (self,name):	#for modifiers
-		if name not in valid_modifiers:
-			raise QueryException(-2)
-		
-		if name in ("w-space","solo"):
-			self.IDcount += 1
-		
-		self.queryElements[str(name)] = True
 	
-	def __setattr__(self,name,value):
-		query_str=""
-		if name.endswith("ID"):
-			self.IDcount += 1
+	def limit (self,limit):
+		if singletonValidator(limit):
+			self.queryElements["limit"] = limit
+		else:
+			raise QueryException(-5)
+			
+	def page (self,page):
+		if singletonValidator(page):
+			self.queryElements["page"] = page
+		else:
+			raise QueryException(-5)
+	
+	def year (self,year):
+		if singletonValidator(year):
+			self.queryValidator["year"] = year
+		else:
+			raise QueryException(-5)
+			
+	def month(self,month):
+		if singletonValidator(month):
+			self.queryValidator["month"] = month
+		else:
+			raise QueryException(-5)
+			
+	def week (self,week):
+		if singletonValidator(week):
+			self.queryValidator["week"] = week
+		else:
+			raise QueryException(-5)
+			
+	def beforeKillID (self,killID):
+		self.IDcount +=1	
+		self.queryValidator["beforeKillID"] = idValidator(killID)
 		
-			if isinstance(value, six.string_types):	#str
-				tmp_list = value.split(',')
-				if len tmp_list > subquery_limit:
-					raise QueryException(-4)
-				
-				for testID in tmp_list:
-					if testID.isdigit():
-						continue
-					else:
-						raise QueryException(-5)
-					
-				query_str = value
-
-			elif type(value) is list:	#list
-				if len(value) > subquery_limit:
-					raise QueryException(-4)
-				
-				for testID in value:
-					if isinstance(testID,int):
-						continue
-					else:
-						raise QueryException(-5)
-				
-				query_str = ','.join(value)
-				
-			else:	#int
-				query_str = str(value)
-				
-		else:	#not ID query (year,month,day,page,etc)
-			if isinstance(value, six.string_types):
-				if value.isdigit():
-					query_str = value
-				else:
+	def afterKillID (self,killID):
+		self.IDcount +=1	
+		self.queryValidator["afterKillID"] = idValidator(killID)
+		
+	def pastSeconds (self,seconds):
+		self.IDcount +=1	
+		self.queryValidator["pastSeconds"] = idValidator(seconds)
+		
+	def characterID (self,characterID):
+		self.IDcount +=1	
+		self.queryValidator["characterID"] = idValidator(characterID)
+		
+	def corpoartionID (self,corporationID):
+		self.IDcount +=1	
+		self.queryValidator["corpoartionID"] = idValidator(corporationID)
+		
+	def allianceID (self,allianceID):
+		self.IDcount +=1	
+		self.queryValidator["allianceID"] = idValidator(allianceID)
+		
+	def factionID (self,factionID):
+		self.IDcount +=1	
+		self.queryValidator["factionID"] = idValidator(factionID)
+		
+	def shipTypeID (self,shipTypeID):
+		self.IDcount +=1	
+		self.queryValidator["shipTypeID"] = idValidator(shipTypeID)
+		
+	def groupID (self,groupID):
+		self.IDcount +=1	
+		self.queryValidator["groupID"] = idValidator(groupID)
+		
+	def solarSystemID (self,solarSystemID):
+		self.IDcount +=1
+		self.queryValidator["solarSystemID"] = idValidator(solarSystemID)
+		
+	def regionID (self,regionID):
+		self.IDcount +=1
+		self.queryValidator["regionID"] = idValidator(regionID)
+		
+	def singletonValidator (self,value):
+		valid = False
+		if isinstance(value,str):
+			if value.isdigit():
+				valid = True
+		elif isinstance(value,int):
+			valid = True
+		return valid
+		
+	def idValidator (self,value):
+		returnstr = ""
+		if isinstance(value,str):
+			tmp_list = value.split(',')
+			valid_list = True
+			for individual in tmp_list:
+				if singletonValidator(individual) == False:
 					raise QueryException(-5)
 					
-			elif type(value) is list:	#list
-				raise QueryException(-5)	#probably need a new exception
-			elif isinstance(value,int):
-				query_str = str(value)
-			else:
-				raise QueryException(-5)	#probably need a new exception
-		self.queryElements[str(name)] = query_str
+			returnstr = value
+		elif isistance(value,int):
+			returnstr = str(value)
+			
+		elif type(value) is list:
+			valid_list = True
+			for individual in value:
+				if singletonValidator(individual) == False:
+					raise QueryException(-5)
+					
+			returnstr = ','.join(str(x) for x in value)
+		return returnstr
+		
+	def __getattr__ (self,name):	#for modifiers
+		mod_str = name.replace('_','-')
+		print mod_str
+		if mod_str not in valid_modifiers:
+			raise QueryException(-2)
+		
+		
+		if mod_str in ("w-space","solo"):
+			self.IDcount += 1
+		
+		self.queryElements[str(mod_str)] = True
 		
 	def __str__ (self):
 		if IDcount < 2:
 			raise QueryException(-1)
-		query_modifiers = ""
+		query_modifiers = self.queryArgs
 		for key,value in self.queryElements.iteritems():
 			if value == True:
 				query_modifiers = "%s/%s" % (key,query_modifiers)	#fetch modifiers must be first
@@ -165,3 +228,17 @@ class Query():
 				query_modifiers = "%s%s/%s/" % (query_modifiers,key,value)
 				
 		return "%s%s" % (self.address,query_modifiers)
+		
+def main():
+	newQuery = Query()
+	newQuery2 = Query("api-only/characterID/628592330/losses/")
+	
+	newQuery.api_only
+	newQuery.characterID(628592330)
+	newQuery.losses
+	
+	print newQuery
+	print newQuery2
+	
+if __name__ == "__main__":
+	main()	
