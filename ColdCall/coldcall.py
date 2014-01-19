cf733734717d40e6f51e9a444f140c50cb046bd5
John Purcell <locke.renard@gmail.com>
2014-01-19 13:06:30 -0700

    added main to coldcall for development

--- ColdCall/coldcall.py
+++ ColdCall/coldcall.py
@@ -5,3 +5,12 @@ import urllib2
 import MySQLdb
 import ConfigParser
 from datetime import datetime
+
+from eveapi import eveapi
+import zkb
+
+def main():
+	test=1
+	
+if __name__ == "__main__":
+	main()
