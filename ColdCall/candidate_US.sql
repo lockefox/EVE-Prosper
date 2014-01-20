28cff79da5aff0b701346355c7284440a59c615f
lockefox <locke.renard@gmail.com>
2014-01-20 10:34:32 -0700

    fixed a broken query range

--- ColdCall/candidate_US.sql
+++ ColdCall/candidate_US.sql
@@ -6,7 +6,7 @@ FROM kill_participants kills
 JOIN invTypes typ on (kills.shipTypeID = typ.typeID)
 JOIN invGroups grp on (typ.groupID = grp.groupID)
 WHERE grp.categoryID <> 350001
-AND ((TIME(kills.kill_time) > '01:00' AND TIME(kills.kill_time))
+AND ((TIME(kills.kill_time) > '01:00' AND TIME(kills.kill_time) < '05:00')
 	OR (WEEKDAY(kills.kill_time) = 5 AND (TIME(kills.kill_time) > '18:00'))
 	OR (WEEKDAY(kills.kill_time) = 6 AND (TIME(kills.kill_time) < '05:00'
 		AND TIME(kills.kill_time) > '18:00'))
