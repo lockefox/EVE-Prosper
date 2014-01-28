78a52029f806c17a839284f1b8c1401b29328224
lockefox <locke.renard@gmail.com>
2014-01-28 10:15:29 -0700

    moved ColdCall files to new folder
    ColdCall moved to its own project: github.com/lockefox/coldcall

---
+++ KillReporter/candidate_US.sql
@@ -0,0 +1,19 @@
+SELECT kills.characterID,
+	SUM(IF(kills.isVictim=1,1,0)) as `losses`,
+	SUM(IF(kills.isVictim=0,1,0)) as `kills`,
+	MAX(kills.kill_time) as `latestActivity`
+FROM kill_participants kills
+JOIN invTypes typ on (kills.shipTypeID = typ.typeID)
+JOIN invGroups grp on (typ.groupID = grp.groupID)
+WHERE grp.categoryID <> 350001
+AND ((TIME(kills.kill_time) > '01:00' AND TIME(kills.kill_time) < '05:00')
+	OR (WEEKDAY(kills.kill_time) = 5 AND (TIME(kills.kill_time) > '18:00'))
+	OR (WEEKDAY(kills.kill_time) = 6 AND (TIME(kills.kill_time) < '05:00'
+		AND TIME(kills.kill_time) > '18:00'))
+	OR (WEEKDAY(kills.kill_time) = 0 AND (TIME(kills.kill_time) < '05:00')))
+AND kills.corporationID = 1000181
+AND kills.kill_time > NOW() - INTERVAL 20 DAY
+GROUP BY kills.characterID
+HAVING losses > 5 OR kills > 5
+AND latestActivity > NOW() - INTERVAL 7 DAY
+ORDER BY losses DESC 
