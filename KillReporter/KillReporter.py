f5967624e40326065708756a596228e36c1f4c5a
lockefox <locke.renard@gmail.com>
2014-01-28 10:58:11 -0700

    adjusted kill_participants to be more complete
    removed coldcall specific code

---
+++ KillReporter/KillReporter.py
@@ -0,0 +1,10 @@
+#!/Python27/python.exe
+
+import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
+import urllib2
+import MySQLdb
+import ConfigParser
+from datetime import datetime, timedelta
+
+from eveapi import eveapi
+import zkb
--- KillReporter/kill_participants.sql
+++ KillReporter/kill_participants.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `kill_participants`;
-CREATE TABLE `kill_participants` (
+DROP TABLE IF EXISTS `kill_participants2`;
+CREATE TABLE `kill_participants2` (
 	`killID` int(32) NOT NULL,
 	`solarSystemID` int(16) NOT NULL,
 	`kill_time` datetime NOT NULL,
@@ -7,11 +7,17 @@ CREATE TABLE `kill_participants` (
 	`shipTypeID` int(8) NOT NULL,
 	`damage` int(8) NOT NULL,
 	`characterID` int(16) NOT NULL,
+	`characterName` tinytext(255) NOT NULL,
 	`corporationID` int(16) NOT NULL,
+	`corporationName` tinytext(255) NOT NULL,
 	`allianceID` int(16) DEFAULT NULL,
+	`allianceName` tinytext(255) DEFAULT NULL,
 	`factionID` int(16) DEFAULT NULL,
+	`factionName` tinytext(255) DEFAULT NULL,
 	`finalBlow` tinyint(1) DEFAULT NULL,
 	`weaponTypeID` int(8) DEFAULT NULL,
+	`points` int(8) DEFAULT NULL,
+	`totalValue` decimal(13,2) DEFAULT NULL,
 	-- `fit_json` mediumtext DEFAULT NULL,
 	PRIMARY KEY(killID,characterID))
 ENGINE=InnoDB DEFAULT CHARSET=latin1
